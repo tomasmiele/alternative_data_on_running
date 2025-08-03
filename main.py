69a6b2cbe6cbd75b95e5e483c0ad2effb4c367b5
Tomas Miele <tomasrm@al.insper.edu.br>
2025-08-03 19:22:56 -0300

    coleta de dados ativada

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ def load_json(filename):
         return json.load(f)
 
 if __name__ == "__main__":
-    # data = {}
-    # companies = ["On", "Hoka", "Nike", "Adidas", "New Balance"]
+    data = {}
+    companies = ["On", "Hoka", "Nike", "Adidas", "New Balance"]
 
-    # for company in companies:
-    #     data[company] = {"M": {}, "F": {}}
+    for company in companies:
+        data[company] = {"M": {}, "F": {}}
 
-    # driver = start_browser()
-    # data["On"]["M"] = get_reviews(driver, "on", "m")
-    # data["On"]["F"] = get_reviews(driver, "on", "f")
-    # data["Hoka"]["M"] = get_reviews(driver, "hoka", "m")
-    # data["Hoka"]["F"] = get_reviews(driver, "hoka", "f")
-    # data["Nike"]["M"] = get_reviews(driver, "nike", "m")
-    # data["Nike"]["F"] = get_reviews(driver, "nike", "f")
-    # data["Adidas"]["M"] = get_reviews(driver, "adidas", "m")
-    # data["Adidas"]["F"] = get_reviews(driver, "adidas", "f")
-    # data["New Balance"]["M"] = get_reviews(driver, "new-balance", "m")
-    # data["New Balance"]["F"] = get_reviews(driver, "new-balance", "f")
-    # driver.quit()
+    driver = start_browser()
+    data["On"]["M"] = get_reviews(driver, "on", "m")
+    data["On"]["F"] = get_reviews(driver, "on", "f")
+    data["Hoka"]["M"] = get_reviews(driver, "hoka", "m")
+    data["Hoka"]["F"] = get_reviews(driver, "hoka", "f")
+    data["Nike"]["M"] = get_reviews(driver, "nike", "m")
+    data["Nike"]["F"] = get_reviews(driver, "nike", "f")
+    data["Adidas"]["M"] = get_reviews(driver, "adidas", "m")
+    data["Adidas"]["F"] = get_reviews(driver, "adidas", "f")
+    data["New Balance"]["M"] = get_reviews(driver, "new-balance", "m")
+    data["New Balance"]["F"] = get_reviews(driver, "new-balance", "f")
+    driver.quit()
 
-    # save_json(data, "data")
+    save_json(data, "data")
 
     data = load_json("data")
 
